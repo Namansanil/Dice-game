b48221396de30052cf5383bfa3739d394f217c92
Namansanil <surfernaman@gmail.com>
2024-08-08 11:53:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var randomnumber2 = Math.floor(Math.random(1, 6)* 6 + 1);
 var randomdice2 =("./dice" + randomnumber2 + ".png");
 document.querySelectorAll("img")[1].setAttribute("src", randomdice2);
 
-if(randomedice < randomdice2){
+if(randomedice > randomdice2){
     document.querySelector("h1").innerHTML ="player 1 won!!!!!";
-}else if(randomdice2 <randomedice){
+}else if(randomdice2 > randomedice){
     document.querySelector("h1").innerHTML ="player 2 won!!!!!";
 }else{
     document.querySelector("h1").innerHTML ="OHH its a tie......";
